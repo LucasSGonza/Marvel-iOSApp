912b580b6e0376488be553d1602a71ec013f7203
LucasSGonza <lucassilva.goncalves@hotmail.com>
2023-12-13 16:06:44 -0300

    first commit

---
+++ Marvel-iOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel-iOSApp
+//
+//  Created by Squad Apps on 13/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
